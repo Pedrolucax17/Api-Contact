53e2d2bffb148adbb9a8edf009c3f67e14b080d9
Pedro Lucas <pl.lucassouza@gmail.com>
2024-05-29 09:00:37 -0300

    implementing token generation service

--- contact/src/main/java/br/com/fiap/contact/config/security/TokenService.java
+++ contact/src/main/java/br/com/fiap/contact/config/security/TokenService.java
@@ -1,10 +1,58 @@
 package br.com.fiap.contact.config.security;
 
+import br.com.fiap.contact.model.UserModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
     @Value("${my.secret.key}")
     private String secretWord;
+
+    public String generateToken(UserModel user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secretWord);
+            String token = JWT
+                    .create()
+                    .withIssuer("contacts")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }
+        catch (JWTCreationException e){
+            throw new RuntimeException("Não foi possível gerar o token");
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secretWord);
+            return JWT
+                    .require(algorithm)
+                    .withIssuer("contacts")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }
+        catch (JWTVerificationException e){
+            return "";
+        }
+    }
+
+    public Instant generateExpirationDate(){
+        return LocalDateTime
+                .now()
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
 }
